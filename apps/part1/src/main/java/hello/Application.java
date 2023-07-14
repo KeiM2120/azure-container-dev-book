93168b2e754cd0ebd89d379c9cffa3cc430e69d7
KeiM2120 <kei.mmpublic@gmail.com>
2023-07-14 23:06:47 +0900

    handson: trigger CI workflow

--- apps/part1/src/main/java/hello/Application.java
+++ apps/part1/src/main/java/hello/Application.java
@@ -14,6 +14,12 @@ public class Application {
 		return "Hello Docker World";
 	}
 
+	@RequestMapping("/hey")
+	public String hey() {
+		String msg = "Hey yo";
+		return msg;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
